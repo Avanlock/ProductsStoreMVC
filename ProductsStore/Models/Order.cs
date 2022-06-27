b2ae04614e687460123ae322220ad64a0ef0a9cd
Sanzhar Mukatay <sanzhar.mukatay@gmail.com>
2022-06-27 17:56:00 +0600

    Создал модель заказа

---
+++ ProductsStore/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ProductsStore.Models;
+
+namespace ProductsStore.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string ContactPhone { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}

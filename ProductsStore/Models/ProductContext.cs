08c6e00ffb8b3e367fc2d33c617a81f5e0708e08
Sanzhar Mukatay <sanzhar.mukatay@gmail.com>
2022-06-27 17:57:07 +0600

    #1 Создал контекст продукта

---
+++ ProductsStore/Models/ProductContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductsStore.Models
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+        {
+            
+        }
+    }
+}

f34059aa393e921473e345543aa7a7e6c02c7d94
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-07-04 19:17:27 +0600

    Добавил контроллер для валидации

---
+++ ProductsStore/Controllers/ValidationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using ProductsStore.Models;
+
+namespace ProductsStore.Controllers
+{
+    public class ValidationController : Controller
+    {
+        private readonly ProductContext _context;
+
+        public ValidationController(ProductContext context)
+        {
+            _context = context;
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        public bool CheckDate(DateTime foundationDate) => foundationDate.Year > 100 && foundationDate < DateTime.Now;
+
+        [AcceptVerbs("GET", "POST")]
+        public bool CheckBrand(string name) => !_context.Brands.ToList().Any(b => b.Name.Equals(name, StringComparison.InvariantCultureIgnoreCase));
+        }
+}

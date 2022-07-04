b90c27a3ec7fa6ea6259ae38f764222bcbd61ca3
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-07-04 19:19:15 +0600

    Добавил в контекст отзывы юзеров

--- ProductsStore/Models/ProductContext.cs
+++ ProductsStore/Models/ProductContext.cs
@@ -8,6 +8,7 @@ namespace ProductsStore.Models
         public DbSet<Order> Orders { get; set; }
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<UserFeedback> UserFeedbacks { get; set; }
         
         public ProductContext(DbContextOptions<ProductContext> options) : base(options)
         {

be489093b62d8d46f5d9cec8772d42f1f96d607b
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-07-04 19:21:42 +0600

    Добавил поля почта и дата основания

--- ProductsStore/Models/Brand.cs
+++ ProductsStore/Models/Brand.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ProductsStore.Models
 {
@@ -6,7 +8,15 @@ namespace ProductsStore.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Заполните поле!")]
+        [Remote("CheckBrand", "Validation", ErrorMessage = "Такой бренд уже есть")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Заполните поле!")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некорректный адрес электронной почты")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Заполните поле!")]
+        [Remote("CheckDate", "Validation", ErrorMessage = "Дата введена некорректно!")]
+        public DateTime FoundationDate{ get; set; }
     }
 }
+

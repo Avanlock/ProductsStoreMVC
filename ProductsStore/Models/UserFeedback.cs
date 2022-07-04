606dfce2414926c146641bcdcada1cbf05b2ee2e
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-07-04 19:15:59 +0600

    Добавил модель для отзывов юзеров

---
+++ ProductsStore/Models/UserFeedback.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductsStore.Models
+{
+    public class UserFeedback
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Заполните поле!")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Заполните поле!")]
+        public string Feedback { get; set; }
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }
+        [Required(ErrorMessage = "Заполните поле!")]
+        public int ProductEvaluation { get; set; }
+    }
+}

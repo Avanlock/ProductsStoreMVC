07038a79c1263c272a81689223cc079052ef2441
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-07-07 14:43:58 +0600

    Добавил энам с названиями сортировки

---
+++ ProductsStore/Enums/SortState.cs
@@ -0,0 +1,16 @@
+﻿namespace ProductsStore.Enums
+{
+    public enum SortState
+    {
+        NameAsc,    
+        NameDesc,
+        BrandAsc, 
+        BrandDesc,
+        DateAsc,
+        DateDesc,
+        CategoryAsc,
+        CategoryDesc,
+        PriceAsc,
+        PriceDesc
+    }
+}

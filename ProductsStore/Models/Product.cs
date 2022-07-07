09f313f2ea3b2e14363696768111b300e59bbd24
Sanzhar Mukatay <sanzhar.mukatay@gmail.com>
2022-07-07 13:24:42 +0600

    Доабвил валидации на имя товара, цену, категорию , бренда

--- ProductsStore/Models/Product.cs
+++ ProductsStore/Models/Product.cs
@@ -10,17 +10,20 @@ namespace ProductsStore.Models
         [Key]
         public int Id { get; set; }
         [Required(ErrorMessage = "Заполните поле!")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Cлишком короткое имя!")]
+
         public string Name { get; set; }
         [Required(ErrorMessage = "Заполните поле!")]
-        [Range(1, int.MaxValue, ErrorMessage = "Товар не может быть бесплатным")]
+        [Range(50, int.MaxValue, ErrorMessage = "Товар не может стоить меньше 50 тг")]
         public decimal Price { get; set; }
-        public string CreateDate { get; set; } = DateTime.Now.ToString("dd/MM/yyyy/ HH:mm");
+        public string CreateDate { get; set; }
         public string UpdateDate { get; set; }
-        [Required(ErrorMessage = "Заполните поле!")]
         public string ImgUrl { get; set; }
+        [Required(ErrorMessage = "Выберите категорию!")]
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public virtual Category Category { get; set; }
+        [Required(ErrorMessage = "Выберите бренд!")]
         public int BrandId { get; set; }
         [ForeignKey("BrandId")]
         public virtual Brand Brand { get; set; }

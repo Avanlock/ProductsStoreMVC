a92d3ee4c991dcbb227f3b2d93346b30c1f8208b
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-07-20 19:52:44 +0600

    Добавил дата сид с данными админа

--- ProductsStore/Models/ProductContext.cs
+++ ProductsStore/Models/ProductContext.cs
@@ -9,10 +9,20 @@ namespace ProductsStore.Models
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<UserFeedback> UserFeedbacks { get; set; }
-        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
         public ProductContext(DbContextOptions<ProductContext> options) : base(options)
         {
             
         }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Role>().HasData(new Role {Id = 1, Name = "admin"});
+            modelBuilder.Entity<Role>().HasData(new Role {Id = 2, Name = "user"});
+            modelBuilder.Entity<User>().HasData(new User {Id = 1, UserName = "Admin", Email = "admin@admin.com", Password = "1234", RoleId = 1});
+        }
+
     }
 }

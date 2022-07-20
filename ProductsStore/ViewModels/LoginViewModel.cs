ed6ce7a70ffce65438abb0be1222a8233dc67e8b
*Sanzhar <*sanzhar.mukatay@gmail.com*>
2022-07-20 19:51:21 +0600

    Добавил частичное представление для модели логин

---
+++ ProductsStore/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsStore.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Не указан Email")]
+        public string Email { get; set; }
+        public string UserName { get; set; }
+   
+        [Required(ErrorMessage = "Не указан пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
